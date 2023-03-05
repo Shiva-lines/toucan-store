8691fb2f3f350b1efed7a2fe154c8950a61e80be
shiva <shiva@dev.dev>
2023-03-05 11:47:59 +0200

    added link to all pages, created views for pages, created new templates: login, register, account

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . views import *
 
 urlpatterns = [
-    path('', MainPage.as_view(), name='main')
+    path('', home, name='home'),
+    path('privacytools', privacytools, name='privacytools'),
+    path('faq', faq, name='faq'),
+    path('blog', blog, name='blog'),
+    path('account', account, name='account'),
+    path('login', login, name='login'),
+    path('register', register, name='register'),
 ]
+
+handler404 = error_404
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.views import generic
+from django.shortcuts import render
+ 
 
 # Create your views here.
 
-class MainPage(generic.ListView):
-    template_name = 'home.html'
+def home(request):
+    return render(request, 'home.html')
 
-    def get_queryset(self):
-        pass
+def privacytools(request):
+    return render(request, 'privacytools.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def account(request):
+    return render(request, 'account.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def error_404(request, exception):
+    return render(request, '404.html')
